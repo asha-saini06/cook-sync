7ba863cd7cc8740ae4826fca810121774273b8d9
Asha Saini <sainiasha.ash@gmail.com>
2025-02-22 19:26:36 +0530

    initialize frontend with React, Vite, and daisyUI

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import Sidebar from "./components/Sidebar";
+import HomePage from "./pages/HomePage";
+
+function App() {
+  return (
+    <div className="flex">
+      <Sidebar />
+      <HomePage />
+    </div>
+  )
+}
+
+export default App;
